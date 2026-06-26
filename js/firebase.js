00dcf3384041421317255cded3617db64a1c5e23
Reubenmaste <spyinfiltrado@gmail.com>
2026-06-25 21:22:30 -0600

    Se agrogo lista en select

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyDe654IyqXUVJvYLV8rsvHrl4bE8Y4acc8",
+    authDomain: "ubereatsyahir.firebaseapp.com",
+    projectId: "ubereatsyahir",
+    storageBucket: "ubereatsyahir.firebasestorage.app",
+    messagingSenderId: "117023640714",
+    appId: "1:117023640714:web:d01dc65c5142a86687b173"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
+  const db= firebase.firestore();
--- js/pedidos.js
+++ js/pedidos.js
@@ -1,9 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const menus = document.querySelectorAll('.side-menu');
+        M.Sidenav.init(menus, {edge:'right'});
+});
+
+
 let contenidoLista ='';
 
+db.collection("platillos").onSnapshot((datos) =>{
+    datos.docChanges().forEach((registro) => {
+        if (registro.type === "added") {
+            agregarALista(registro.doc.data(),registro.doc.id);
+        }
+    });
+    var elems = document.querySelectorAll('select');
+    M.formSelect.init(elems);
+
+});
+
 function agregarALista(platillo, id) {
-    contenidoLista = `<option value=''>;
-    ${mostrarPlatillo.nombre}
+    contenidoLista += `<option value='${id}'>;
+    ${platillo.nombre}
     </option>`;
-    document.getElementById('ListaPlatillos').innerHTML =
-        ContenidoLista;
+    document.getElementById('listaPlatillos').innerHTML=contenidoLista;
 }
+M.AutoInit();
