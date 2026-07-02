ee7eb694faa3131000862d9c23629fffb7bf14ef
Reubenmaste <spyinfiltrado@gmail.com>
2026-07-01 21:23:30 -0600

    El agregado lista muestra los archivos

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    const menus = document.querySelectorAll('.side-menu');
-        M.Sidenav.init(menus, {edge:'right'});
+  // nav menu
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, {edge: 'right'});
+    const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, {edge: 'left'});
 });
-
 
 let contenidoLista ='';
 
-db.collection("platillos").onSnapshot((datos) =>{
-    datos.docChanges().forEach((registro) => {
-        if (registro.type === "added") {
-            agregarALista(registro.doc.data(),registro.doc.id);
-        }
-    });
-    var elems = document.querySelectorAll('select');
-    M.formSelect.init(elems);
+db.collection("platillos").onSnapshot((datos) => {
+  datos.docChanges().forEach((registro) => {
+    if (registro.type === "added"){
+      agregarALista(registro.doc.data(),registro.doc.id);
+    } 
+  });
+  var elems = document.querySelectorAll('select');
+  M.FormSelect.init(elems);
 
-});
+})
 
-function agregarALista(platillo, id) {
-    contenidoLista += `<option value='${id}'>;
-    ${platillo.nombre}
-    </option>`;
-    document.getElementById('listaPlatillos').innerHTML=contenidoLista;
+function agregarALista(platillo, id){
+contenidoLista += `<option value='${id}'>
+
+${platillo.nombre} </option>`;
+document.getElementById('listaPlatillos').innerHTML=contenidoLista;
 }
 M.AutoInit();
+
